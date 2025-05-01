c378bf16da2888f6370099dee1c105e7bd409a2d
Anu Tao <anuhea@umich.edu>
2025-05-01 18:17:39 -0400

    added description

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import followreina from './following-reina.MOV';
 import tipping from './tipping-test.mov';
+import robot from './robot_covering.jpg';
 
 function App() {
   return (
@@ -17,6 +18,32 @@ function App() {
       <a href="https://docs.google.com/presentation/d/1Lk74Ovz5xczITs0ZbWS5hNM_pjBpBkDaTUhyTTMFSSs/edit?usp=drive_link" target="_blank" rel="noopener noreferrer" className="btn btn-outline-dark">Expo Slides</a>
       <a href="https://github.com/anujtaosf/ROB450_all_code.git" target="_blank" rel="noopener noreferrer" className="btn btn-outline-dark">GitHub Repo</a>
       </div>
+
+
+        <hr style={{ borderTop: '5px dotted #666', margin: '2.5rem 0' }} />
+
+        <div className="d-flex justify-content-center align-items-center mb-5">
+          <img
+            src={robot}
+            width="300"
+            alt="Robot Overview"
+            className="me-4"
+            style={{ maxWidth: '100%', height: 'auto' }}
+          />
+          <div style={{ maxWidth: '600px'}}>
+            <h5>Robot Introduction</h5>
+            <p>
+              Shortbot is a compact, human-height robotic platform designed to navigate crowded social environments with minimal disruption. Our goal with Shortbot was to create a working prototype that is able to traverse from an arbitrary point A to point B, some distance away. This prototype 
+              will serve to show that a platform of this stature is feasible and has the possibility to enable researchers to conduct more realistic experiments and studies.
+              Our final design stands at 1.24 meters (4.08 ft) tall with a total footprint less than 0.1156 square meters. Shortbot features a Realsense camera for navigation, 
+              lights for visual and directional cues, omnidirectional 
+              wheels, and reinforced mechanical supports to meet inertial stability requirements. While the ETH Zurich Ballbot served as our foundational platform, we 
+              continuously refined the mechanical design throughout the semester, resulting in several visual and structural changes. The final system can be broken down 
+              into three parts: the original ETH Zurich ballbot-inspired base, the mechanical upgrades we introduced, and the additional hardware components we integrated to 
+              create a more intelligent and legible robot.
+            </p>
+          </div>
+        </div>
 
 
         <hr style={{ borderTop: '5px dotted #666', margin: '2.5rem 0' }} />
