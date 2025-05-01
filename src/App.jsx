5d367855b4b12cc2d51c8f4b7e55d6d1e05dcd69
Anu Tao <anuhea@umich.edu>
2025-05-01 18:19:26 -0400

    small edit

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,11 @@ function App() {
           <div style={{ maxWidth: '600px'}}>
             <h5>Robot Introduction</h5>
             <p>
-              Shortbot is a compact, human-height robotic platform designed to navigate crowded social environments with minimal disruption. Our goal with Shortbot was to create a working prototype that is able to traverse from an arbitrary point A to point B, some distance away. This prototype 
+              Short-Bot is a compact, human-height robotic platform designed to navigate crowded social environments with minimal disruption. Our goal with Short-Bot was to create a working prototype that is able to traverse from an arbitrary point A to point B, some distance away. This prototype 
               will serve to show that a platform of this stature is feasible and has the possibility to enable researchers to conduct more realistic experiments and studies.
-              Our final design stands at 1.24 meters (4.08 ft) tall with a total footprint less than 0.1156 square meters. Shortbot features a Realsense camera for navigation, 
+              Our final design stands at 1.24 meters (4.08 ft) tall with a total footprint less than 0.12 square meters. Short-Bot also features a Realsense camera for navigation, 
               lights for visual and directional cues, omnidirectional 
-              wheels, and reinforced mechanical supports to meet inertial stability requirements. While the ETH Zurich Ballbot served as our foundational platform, we 
+              wheels, and reinforced mechanical supports. While the ETH Zurich Ballbot served as our foundational platform, we 
               continuously refined the mechanical design throughout the semester, resulting in several visual and structural changes. The final system can be broken down 
               into three parts: the original ETH Zurich ballbot-inspired base, the mechanical upgrades we introduced, and the additional hardware components we integrated to 
               create a more intelligent and legible robot.
